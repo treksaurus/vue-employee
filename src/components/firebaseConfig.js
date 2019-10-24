9793be6330bcc37efbaab01f8f1bfdc672d433a3
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-10-24 10:55:08 +0700

    initial

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCwcipV7cRex1EwpwvFFH3fG9U7n4AKxfY",
+    authDomain: "vuefs-prod-9a116.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-9a116.firebaseio.com",
+    projectId: "vuefs-prod-9a116",
+    storageBucket: "vuefs-prod-9a116.appspot.com",
+    messagingSenderId: "768852363912",
+    appId: "1:768852363912:web:10e21a6aa41c52a1072d3d",
+    measurementId: "G-WD0Z1G0EYD"
+}
